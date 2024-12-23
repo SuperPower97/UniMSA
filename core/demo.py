4a155d0c9d99b2f3f3f8efd684eb49d00f651420
Yutao Wei <93330052+SuperPower97@users.noreply.github.com>
2024-12-23 13:41:36 +0800

    Delete core/demo.py

--- core/demo.py
+++
@@ -1,9 +0,0 @@
-import torch
-import ipdb
-import torch.nn as nn
-
-
-checkpoint_path = './ckpt/mosi/lambda_0.1/best_Corr_1111.pth'  # 替换为您的 .pth 文件路径
-check = torch.load(checkpoint_path)
-keys = check['state_dict'].keys()
-ipdb.set_trace()
